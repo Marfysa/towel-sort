57a650bec39f123de9b0d7b2a561d0c41dab9bec
Maria Kurakevich <marfysa14@gmail.com>
2021-08-10 18:49:18 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@
 
 module.exports = function towelSort (matrix) {
   if (matrix == undefined || matrix.length == 0) return [];
+  if (matrix.length === 1) {
+    return matrix;
+  } 
   let i = 0;
   let revArr = [];
   let result = [];
